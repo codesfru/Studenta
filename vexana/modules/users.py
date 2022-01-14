a200257761955dd06568808b5ad34ad5079ecf71
CODESFRU <97609617+codesfru@users.noreply.github.com>
2022-01-14 18:01:33 +0530

    Update users.py

--- vexana/modules/users.py
+++ vexana/modules/users.py
@@ -11,10 +11,11 @@ from telegram.ext import (
     run_async,
 )
 
-import SaitamaRobot.modules.sql.users_sql as sql
-from SaitamaRobot import DEV_USERS, LOGGER, OWNER_ID, dispatcher
-from SaitamaRobot.modules.helper_funcs.chat_status import dev_plus, sudo_plus
-from SaitamaRobot.modules.sql.users_sql import get_all_users
+import vexana.modules.sql.users_sql as sql
+from vexana import DEV_USERS, LOGGER, OWNER_ID, dispatcher
+from vexana.modules.helper_funcs.chat_status import dev_plus, sudo_plus
+from vexana.modules.sql.users_sql import get_all_users
+
 
 USERS_GROUP = 4
 CHAT_GROUP = 5
